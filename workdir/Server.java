cd9b81c65dca0944630b5a3f38670fe0196fb0fd
lhan5426 <lawrench@andrew.cmu.edu>
2021-04-01 16:37:34 -0400

    temp change for graph

--- workdir/Server.java
+++ workdir/Server.java
@@ -97,7 +97,7 @@ public class Server {
 
 		}
 		int booted = 1;
-		int temp = 0;
+		int temp = 1;
 		//designated master node, started required # of VM;s
 		if (id == 1) {
 			for (int i = 0; i < temp; i ++) {
