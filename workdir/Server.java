e4ea50f09ad539cd0b6a4e1e1988e21252b93790
lhan5426 <lawrench@andrew.cmu.edu>
2021-04-01 11:28:54 -0400

    trying to find lowest bound for init hours

--- workdir/Server.java
+++ workdir/Server.java
@@ -11,8 +11,8 @@ import java.time.Instant;
 public class Server {
 
 	public static int[] hardcoded = new int[]{
-		3,2,2,2,
-		2,3,10,10,
+		3,3,3,3,
+		3,3,10,10,
 		10,10,10,10,
 		10,10,10,10,
 		10,10,10,10,
