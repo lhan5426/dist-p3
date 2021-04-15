b5dad106e99c7b089affac76c52d717263436e21
lhan5426 <lawrench@andrew.cmu.edu>
2021-04-15 19:23:37 -0400

    more debugging with the logger

--- workdir/Server.java
+++ workdir/Server.java
@@ -239,6 +239,9 @@ public class Server {
 
 			while (true) {
 				Cloud.FrontEndOps.Request r0 = to_mid.getHead();
+				while (r0 == null) {
+					logger.info(timestamp_log("currently waiting for req to come in\n"));
+				}
 				logger.info("\tcurrent req: " + r0.toString() + "\n");
 				Cloud.FrontEndOps.Request r = to_mid.removeHead();
 				SL.processRequest(r);
