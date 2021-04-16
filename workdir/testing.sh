8f7ebffe81a20ba705c500782084ac6215d450e7
Lawrence Han <lawrench@andrew.cmu.edu>
2021-04-15 20:15:13 -0400

    Adding shell script from andrew end

---
+++ workdir/testing.sh
@@ -0,0 +1,80 @@
+java Cloud 15027 ../lib/db1.txt c-2000-69420 0 60 | grep "^Stats:*" > hour0stats.txt
+{ java Cloud 15027 ../lib/db1.txt u-1000-3000-69420 0 60 | grep "^Stats:*"
+      java Cloud 15027 ../lib/db1.txt u-1000-3000-1 0 60 | grep "^Stats:*"
+        java Cloud 15027 ../lib/db1.txt e-2000-69420 0 60 | grep "^Stats:*"
+} >> hour0stats.txt
+
+#java Cloud 15027 ../lib/db1.txt u-1750-3000-69420 1 60
+#java Cloud 15027 ../lib/db1.txt e-2200-69420 1 60
+#
+##java Cloud 15027 ../lib/db1.txt u-8000-10000-69420 2 60
+#java Cloud 15027 ../lib/db1.txt e-10000-69420 2 60
+#
+##java Cloud 15027 ../lib/db1.txt u-8000-10000-69420 3 60
+#java Cloud 15027 ../lib/db1.txt e-10000-69420 3 60
+#
+##java Cloud 15027 ../lib/db1.txt u-3000-4000-69420 4 60
+#java Cloud 15027 ../lib/db1.txt e-3000-69420 4 60
+#
+##java Cloud 15027 ../lib/db1.txt u-2000-3000-69420 5 60
+#java Cloud 15027 ../lib/db1.txt e-3000-69420 5 60
+#
+##
+#java Cloud 15027 ../lib/db1.txt u-2000-3000-69420 6 60
+#java Cloud 15027 ../lib/db1.txt e-3000-69420 6 60
+#
+##
+#java Cloud 15027 ../lib/db1.txt u-1000-1400-69420 7 60
+#java Cloud 15027 ../lib/db1.txt e-1333-69420 7 60
+#
+##
+#java Cloud 15027 ../lib/db1.txt u-800-1200-69420 8 60
+#java Cloud 15027 ../lib/db1.txt e-1000-69420 8 60
+#
+##
+#java Cloud 15027 ../lib/db1.txt u-1000-1500-69420 9 60
+#java Cloud 15027 ../lib/db1.txt e-1250-69420 9 60
+#
+##java Cloud 15027 ../lib/db1.txt u-1000-1500-69420 10 60
+#java Cloud 15027 ../lib/db1.txt e-1250-69420 10 60
+#
+##java Cloud 15027 ../lib/db1.txt u-1000-1500-69420 11 60
+#java Cloud 15027 ../lib/db1.txt e-1250-69420 11 60
+#
+##java Cloud 15027 ../lib/db1.txt u-800-1000-69420 12
+#java Cloud 15027 ../lib/db1.txt e-1000-69420 12
+#
+##java Cloud 15027 ../lib/db1.txt u-750-1000-69420 13
+#java Cloud 15027 ../lib/db1.txt e-875-69420 13
+#
+##java Cloud 15027 ../lib/db1.txt u-900-1100-69420 14
+#java Cloud 15027 ../lib/db1.txt e-1000-69420 14
+#
+##java Cloud 15027 ../lib/db1.txt u-1166-1500-69420 15
+#java Cloud 15027 ../lib/db1.txt e-1333-69420 15
+#
+##java Cloud 15027 ../lib/db1.txt u-1000-1500-69420 16 60
+#java Cloud 15027 ../lib/db1.txt e-1250-69420 16 60
+#
+##java Cloud 15027 ../lib/db1.txt u-1166-1500-69420 17
+#java Cloud 15027 ../lib/db1.txt e-1333-69420 17
+#
+##java Cloud 15027 ../lib/db1.txt u-800-1000-69420 18
+#java Cloud 15027 ../lib/db1.txt e-1000-69420 18
+#
+##
+#java Cloud 15027 ../lib/db1.txt u-600-900-69420 19
+#java Cloud 15027 ../lib/db1.txt e-750-69420 19
+#
+##
+#java Cloud 15027 ../lib/db1.txt u-600-730-69420 20
+#java Cloud 15027 ../lib/db1.txt e-666-69420 20
+#
+##java Cloud 15027 ../lib/db1.txt u-666-730-69420 21
+#java Cloud 15027 ../lib/db1.txt e-700-69420 21
+#
+##java Cloud 15027 ../lib/db1.txt u-900-1100-69420 22
+#java Cloud 15027 ../lib/db1.txt e-1000-69420 22
+#
+##java Cloud 15027 ../lib/db1.txt u-1000-1500-69420 23 60
+#java Cloud 15027 ../lib/db1.txt e-1250-69420 23 60
